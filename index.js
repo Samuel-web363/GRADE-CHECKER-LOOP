97e4ffef112b3d0463bb73565f1c3248038b9c27
Samuel-web363 <chinonsosamuel2654@gmail.com>
2025-06-22 02:12:04 -1100

    Modified the index.,js file

---
+++ index.js
@@ -0,0 +1,57 @@
+// IF/ELSE STATEMENT 
+const a = 20
+if (a >= 70) {
+    console.log("Excellent")
+} else if (a >= 60 && a <= 69) {
+    console.log("Very Good")
+} else if (a >= 50 && a <= 59) {
+    console.log("Good")
+}
+else if (a >= 40 && a <= 49) {
+    console.log("Pass")
+}
+else if (a >= 30 && a <= 39) {
+    console.log("Fail")
+}else{
+    console.log("Bad")
+}
+
+
+// LOOP ASSIGNMENT
+// JS1-SS3
+const js1 = ["Chioma", "Chinonso" , "Adimchi" , "Samuel" , "Favour"]
+const js2 = ["Olivia","Hart","Ethan", "Blake","Mia",]
+const js3 = ["Zara", "Quinn", "Lucas", "Dean", "Ivy" ]
+const ss1 =["Monroe", "Aiden", "Cross", "Chloe","Rivers"]
+const ss2 = ["Max", "Carter", "Layla", "Brooks", "Jordan"]
+const ss3 = ["Miles","Ava", "Sinclair", "Leo", "Bennett"]
+
+// LOOP FOR JSS1
+for(let b = 0; b < js1.length; b++){
+    console.log(js1[b])
+}
+
+// LOOP FOR JSS2
+for(let b = 0; b < js2.length; b++){
+    console.log(js2[b])
+}
+
+//  LOOP FOR JSS3
+for(let b = 0; b < js3.length; b++){
+    console.log(js3[b])
+}
+
+// LOOP FOR SS1
+for(let b = 0; b < ss1.length; b++){
+    console.log(ss1[b])
+}
+
+// LOOP FOR SS2
+for(let b = 0; b < ss2.length; b++){
+    console.log(ss2[b])
+}
+
+// LOOP FOR SS3
+for(let b = 0; b < ss3.length; b++){
+    console.log(ss3[b])
+}
